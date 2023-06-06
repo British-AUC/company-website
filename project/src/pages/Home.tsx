1b73ddcaecee2e6b1a02973a3f5a8920531f6c96
Tersoo <tersooahire21@gmail.com>
2023-06-06 17:19:21 +0100

    finished flags section

--- project/src/pages/Home.tsx
+++ project/src/pages/Home.tsx
@@ -1,6 +1,18 @@
 import React from "react"
 import '../styles/App.css'
 import heroimage from "../assets/heroimage.webp"
+import ukflag from "../assets/ukflag.svg"
+import usflag from "../assets/usflag.svg"
+import canadaflag from "../assets/canadaflag.svg"
+import irelandflag from "../assets/irelandflag.svg"
+import scotlandflag from "../assets/scotlandflag.svg"
+import australiaflag from "../assets/australiaflag.svg"
+import newzealandflag from "../assets/newzealandflag.svg"
+import germanyflag from "../assets/germanyflag.svg"
+import turkeyflag from "../assets/turkeyflag.svg"
+import cyprusflag from "../assets/cyprusflag.svg"
+import polandflag from "../assets/polandflag.svg"
+// import ukraineflag from "../assets/ukraineflag.svg"
 
 export default function Home(){
     return (
@@ -33,7 +45,20 @@ export default function Home(){
                     We’ve helped over 1000 Nigerian students gain university 
                     admission in over 25 countries across the globe.
                 </p>
-
+                <div className="flags-container">
+                    <img src={ukflag} alt="UK Flag" />
+                    <img src={usflag} alt="US Flag" />
+                    <img src={canadaflag} alt="Canada Flag" />
+                    <img src={irelandflag} alt="Ireland Flag" />
+                    <img src={scotlandflag} alt="Scotland Flag" />
+                    <img src={australiaflag} alt="Australia Flag" />
+                    <img src={newzealandflag} alt="New Zealand Flag" />
+                    <img src={germanyflag} alt="Germany Flag" />
+                    <img src={turkeyflag} alt="Turkey Flag" />
+                    <img src={cyprusflag} alt="Cyprus Flag" />
+                    <img src={polandflag} alt="poland Flag" />
+                    {/* <img src={ukraineflag} alt="Ukraine Flag" /> */}
+                </div>
             </section>
         </>
     )
