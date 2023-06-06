1d66d1c20fec777b421088eaa94b97748fb0e94c
Tersoo <tersooahire21@gmail.com>
2023-06-06 17:00:06 +0100

    created hero section and navgation for the website

--- project/src/components/Navbar.tsx
+++ project/src/components/Navbar.tsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom"
+import phone from "../assets/phone.svg";
+import email from "../assets/email.svg";
+import logo from "../assets/logo.svg";
 
 export default function Navbar(){
+    const location = useLocation();
+
     return (
-        <>
-            <h1>Navigation</h1>
-        </>
+        <nav>
+            <div className="nav-top">
+                <p><img src={phone} alt="Phone Icon" /> +234 (0) 805 900 0097</p>
+                <p><img src={email} alt="Email Icon" /> support@britishauc.com</p>
+            </div>
+
+            <div className="nav-bottom">
+                <div className="nav-bottom-left">
+                    <img src={logo} alt="Logo Icon" />
+                </div>
+                <div className="nav-bottom-right">
+                    <div className={location.pathname === "/" ? "active" : ""}>
+                        <Link to="/">Home</Link>
+                    </div>
+                    <div className={location.pathname === "/about" ? "active" : ""}>
+                        <Link to="/about">About us</Link>
+                    </div>
+                    <div className={location.pathname === "/services" ? "active" : ""}>
+                        <Link to="/services">Services</Link>
+                    </div>
+                    <div className={location.pathname === "/events" ? "active" : ""}>
+                        <Link to="/events">Events</Link>
+                    </div>
+                    <div className={location.pathname === "/blog" ? "active" : ""}>
+                        <Link to="/blog">Blog</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
     )
 }
--- project/src/pages/Home.tsx
+++ project/src/pages/Home.tsx
@@ -1,10 +1,40 @@
 import React from "react"
 import '../styles/App.css'
+import heroimage from "../assets/heroimage.webp"
 
 export default function Home(){
     return (
         <>
-            <h1>Home Page</h1>
+        {/* HERO SECTION */}
+            <header className="hero">
+                <div className="hero-left">
+                    <h1>Want to Study <span style={{color:"#005EBD"}}>Abroad?</span></h1>
+                    <p>
+                        Lets take the stress off you! We will get you into any 
+                        university or college around the world.
+                    </p>
+                    <div className="button-container">
+                        <button className="button-1">
+                            Speak to an advisor
+                        </button>
+                        <button className="button-2">
+                            Learn more
+                        </button>
+                    </div>
+                </div>
+                <div className="hero-right">
+                    <img src={heroimage} alt="Hero Image" />
+                </div>
+            </header>
+
+            {/* SECTION ONE - COUNTRY FLAGS */}
+            <section className="section-one">
+                <p>
+                    We’ve helped over 1000 Nigerian students gain university 
+                    admission in over 25 countries across the globe.
+                </p>
+
+            </section>
         </>
     )
 }
