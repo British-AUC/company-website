e3addc85d8b7471fbd32137d0b984d2277ee94c1
Tersoo <tersooahire21@gmail.com>
2023-06-09 09:36:25 +0100

    added partners section

--- website/src/pages/About.jsx
+++ website/src/pages/About.jsx
@@ -3,6 +3,12 @@ import CEO from "../assets/mrcollins.png";
 import linkedin from "../assets/testimonials/linkedin_icon.svg";
 import { Values1, Values2 } from "../components/Values";
 import { data1, data2 } from "../data/valueprops";
+import partner1 from "../assets/partners/partner1.svg";
+import partner2 from "../assets/partners/partner2.svg";
+import partner3 from "../assets/partners/partner3.svg";
+import partner4 from "../assets/partners/partner4.svg"
+import partner5 from "../assets/partners/partner5.svg"
+
 
 export default function About(){
     const valuepropositon1 = data1.map(item => <Values1 key={item.id} {...item} />)
@@ -69,7 +75,7 @@ export default function About(){
                 </div>
             </section>
 
-            {/* SECTION NINE */}
+            {/* SECTION TEN */}
             <section className="section-ten">
                 <h2>Why choose our Application Center</h2>
                 <div className="section-ten-sub">
@@ -81,6 +87,19 @@ export default function About(){
                     </div>
                 </div>
             </section>
+
+            {/* SECTION ELEVEN */}
+            <section className="section-eleven" >
+                <h2>Our Partners</h2>
+                <div className="images-container">
+                    <img src={partner1} alt="Partners Icon" />
+                    <img src={partner2} alt="Partners Icon" />
+                    <img src={partner3} alt="Partners Icon" />
+                    <img src={partner4} alt="Partners Icon" />
+                    <img src={partner5} alt="Partners Icon" />
+                </div>
+            </section>
+
         </>
     )
 }
