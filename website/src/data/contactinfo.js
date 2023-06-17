9d2a33471d88c8f6912ad3598a8ecabe6615e80f
Ahire Tersoo David <105167338+tersoo-ahire@users.noreply.github.com>
2023-06-17 06:36:25 +0100

    Add files via upload
    replaced data

--- website/src/data/contactinfo.js
+++ website/src/data/contactinfo.js
@@ -6,7 +6,7 @@ export default [
     {
         id: 1,
         icon: phone,
-        title: "Contact Info",
+        title: "General Enquiries",
         content1: "09033942463",
         content2: "contact@britishauc.com",
     },
@@ -20,43 +20,49 @@ export default [
     {
         id: 3,
         icon: location,
-        title: "Abuja Address (HQ)",
+        title: "Abuja (HQ)",
         content1: "No 8 Ihiala Street, off Plateau Street, Area 2, Garki, Abuja, Nigeria.",
         content2: "08059000097",
+        content3: "abuja@britishauc.com"
     },
     {
         id: 4,
         icon: location,
-        title: "Lagos Address",
+        title: "Lagos",
         content1: "56 Opebi Road Ikeja, Lagos, Nigeria.",
         content2: "09133078401",
+        content3: "lagos@britishauc.com"
     },
     {
         id: 5,
         icon: location,
-        title: "Port-Harcourt Address",
-        content1: "Suite 9, Davis Mall, No. 2B General Dairiiya Street, Off Tombia Street Phase 2., Port-Harcourt, Nigeria.",
-        content2: "",
+        title: "Port-Harcourt",
+        content1: "Suite 9, Davis Mall, No. 2B General Dairiiya Street, PH, Nigeria.",
+        content2: "07026444791",
+        content3: "portharcourt@britishauc.com"
     },
     {
         id: 6,
         icon: location,
-        title: "Kano Address",
-        content1: "Maiduguri Road, opposite Al-Yuma House, Tarauni 700223, Kano, Nigeria",
+        title: "Kano",
+        content1: "Maiduguri Road, opposite Al-Yuma House, Tarauni, Kano, Nigeria.",
         content2: "09063832706",
+        content3: "kano@britishauc.com"
     },
     {
         id: 7,
         icon: location,
-        title: "Uyo Address",
-        content1: "No 32B G. Bassey Avenue, Ewet Housing Estate, Uyo, Akwa Ibom, Nigeria.",
+        title: "Uyo",
+        content1: "No. 32B G. Bassey Avenue, Ewet Housing Estate, Uyo, Akwa Ibom, Nigeria.",
         content2: "08075102470",
+        content3: "uyo@britishauc.com"
     },
     {
         id: 8,
         icon: location,
-        title: "Enugu Address",
-        // content1: "No 8 Ihiala Street, off Plateau Street, Area 2, Garki, Abuja, Nigeria.",
-        content2: "08171008731",
+        title: "Ibadan",
+        content1: "No. 49 MKO Abiola way, Ajeigbe, Ring Road, Ibadan, Oyo, Nigeria.",
+        content2: "09138708926",
+        content3: "ibadan@britishauc.com",
     },
 ]
--- website/src/data/events.js
+++ website/src/data/events.js
@@ -1,33 +1,13 @@
-import event1 from "../assets/events/event1.png"
-import event2 from "../assets/events/event2.png"
-import event3 from "../assets/events/event3.png"
+import expoevent from "../assets/events/expoevent.jpg"
 
 export default [
     {
         id: 1,
-        image: event1,
-        title: "BRITISH AUC STUDY TOUR 2023",
-        description: "Attention Delegates! Register your details now for the highly anticipated BritishAUC Study 2023. Showcase your esteemed institution and unlock countless opportunities to connect with Nigerian students aspiring to study abroad. Don't miss your chance to make lasting impressions, build invaluable partnerships, and contribute to shaping the educational landscape. Secure your spot today and be part of a transformative event that opens doors to a world of possibilities. Register now and embark on a remarkable journey of academic excellence and global impact!",
-        date: "September 17th to October 9th 2023",
+        image: expoevent,
+        title: "BRITISH AUC STUDY TOUR 2023",
+        description: "Join the British AUC STUDY TOUR, 2023 from September 17th to October 9th, visiting major cities and over 64 high-profile international secondary schools in Nigeria. Engage with SS3/Final Year students interested in studying abroad and participate in college/university exhibitions. Register now to showcase your institution, connect with Nigerian students, and shape the educational landscape. Don't miss this transformative event of academic excellence and global impact!",
+        date: "September 17th to October 9th 2023",
         // time: "9am",
         link: "/study-tour-2023",
     },
-    // {
-    //     id: 2,
-    //     image: event2,
-    //     title: "Lorem ipsum dolor sit amet consectetur. Pellentesque ut in lorem nunc nam scelerisque.",
-    //     description: "Lorem ipsum dolor sit amet consectetur. Elit nascetur auctor quisque velit. Nullam nibh congue proin felis lorem pulvinar rhoncus condimentum molestie. Dis in tellus amet aenean accumsan tincidunt potenti. Ut consectetur nunc sodales non ut sit. Id est imperdiet amet in sit. Praesent urna phasellus amet bibendum etiam. Elit enim tellus cras auctor adipiscing adipiscing nulla egestas. Viverra ultrices arcu id vitae. Vitae massa risus orci faucibus quis. Nunc pretium fermentum suspendisse at aenean nullam odio hac ac.",
-    //     date: "Tuesday 25th July 2023",
-    //     time: "10am",
-    //     link: "#",
-    // },
-    // {
-    //     id: 3,
-    //     image: event3,
-    //     title: "Lorem ipsum dolor sit amet consectetur. Pellentesque ut in lorem nunc nam scelerisque.",
-    //     description: "Lorem ipsum dolor sit amet consectetur. Elit nascetur auctor quisque velit. Nullam nibh congue proin felis lorem pulvinar rhoncus condimentum molestie. Dis in tellus amet aenean accumsan tincidunt potenti. Ut consectetur nunc sodales non ut sit. Id est imperdiet amet in sit. Praesent urna phasellus amet bibendum etiam. Elit enim tellus cras auctor adipiscing adipiscing nulla egestas. Viverra ultrices arcu id vitae. Vitae massa risus orci faucibus quis. Nunc pretium fermentum suspendisse at aenean nullam odio hac ac.",
-    //     date: "Tuesday 25th July 2023",
-    //     time: "10am",
-    //     link: "#",
-    // },
 ]
