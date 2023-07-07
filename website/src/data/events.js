18a50192aaf8fe38aa3df99958b0fec0fc655569
Ahire Tersoo David <105167338+tersoo-ahire@users.noreply.github.com>
2023-07-07 22:38:17 +0100

    updated event flyers

--- website/src/data/events.js
+++ website/src/data/events.js
@@ -1,11 +1,11 @@
-import expoevent from "../assets/events/expoevent.webp"
-import sportsevent from "../assets/events/sportsevent.webp"
+import studytour from "../assets/events/study_tour.webp"
+import sportsstudytour from "../assets/events/sports_study_tour.webp"
 
 
 export default [
     {
         id: 1,
-        image: expoevent,
+        image: studytour,
         title: "BRITISH AUC STUDY TOUR 2023",
         description: "Join the British AUC STUDY TOUR, 2023 from September 17th to October 9th, visiting major cities and over 64 high-profile international secondary schools in Nigeria. Engage with SS3/Final Year students interested in studying abroad and participate in college/university exhibitions. Register now to showcase your institution, connect with Nigerian students, and shape the educational landscape. Don't miss this transformative event of academic excellence and global impact!",
         date: "September 17th to October 9th 2023",
@@ -14,7 +14,7 @@ export default [
     },
     {
         id: 2,
-        image: sportsevent,
+        image: sportsstudytour,
         title: "BRITISH AUC SPORT & STUDY EXPO 2023",
         description: "Join the Sport & Study Expo 2023 on September 12th, 14th, and 15th in Abuja and Lagos. Connect with high-profile Nigerian secondary schools and showcase your institution's robust sports and study programs. Engage with students and parents seeking sports and study opportunities abroad. Don't miss this event dedicated to high school students pursuing A Level + Sports, International Foundation Year + Sports, International Baccalaureate + Sports, US Diploma + Sports, OSSD + Sports, or Universities with strong sports presence.",
         date: "12th, 14th and 15th September 2023",
