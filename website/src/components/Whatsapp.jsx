1fa33fab7b687da15eed62ecd05f3a2d3fd70223
Ahire Tersoo David <105167338+tersoo-ahire@users.noreply.github.com>
2023-07-12 14:26:20 +0100

    added css transition to whatsapp

--- website/src/components/Whatsapp.jsx
+++ website/src/components/Whatsapp.jsx
@@ -3,7 +3,7 @@ import whatsapp from "../assets/whatsapp.svg"
 
 export default function WhatsApp(){
     return (
-        <div className="whatsapp">
+        <div className="whatsapp" title="Speak to our Customer Representative on Whatsapp">
             <a href="https://wa.link/16iyyk" target="_blank">
                 <img src={whatsapp} alt="WhatsApp Icon" />
             </a>
